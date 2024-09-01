5542c8b8e73ba59914664285fa17bfb5868e7e53
Sam Larsen <sam@slarsen.io>
2024-09-01 05:47:32 -0400

    Cargo fmt

--- src/curves/secp256k1.rs
+++ src/curves/secp256k1.rs
@@ -18,8 +18,16 @@ pub fn process() -> Secp256k1 {
     let public_key_point = ProjectivePoint::GENERATOR * private_key_scalar; // Perform scalar multiplication with the base point of the curve to get the public key point
 
     // Encode the public key point into uncompressed and compressed byte representations
-    let public_key_bytes_65 = public_key_point.to_encoded_point(false).as_bytes().try_into().unwrap(); // Get the uncompressed public key format (65 bytes)
-    let public_key_bytes_33 = public_key_point.to_encoded_point(true).as_bytes().try_into().unwrap(); // Get the compressed public key format (33 bytes)
+    let public_key_bytes_65 = public_key_point
+        .to_encoded_point(false)
+        .as_bytes()
+        .try_into()
+        .unwrap(); // Get the uncompressed public key format (65 bytes)
+    let public_key_bytes_33 = public_key_point
+        .to_encoded_point(true)
+        .as_bytes()
+        .try_into()
+        .unwrap(); // Get the compressed public key format (33 bytes)
 
     // Convert the scalar to its byte representation
     let private_key_bytes_32: [u8; 32] = private_key_scalar.to_bytes().into(); // Convert the private key scalar to a 32-byte array
